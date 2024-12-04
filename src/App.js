4165080ea1f33878580d511148946067a966257d
Amit pandey <amitpandeye120@gmail.com>
2024-12-04 16:54:04 +0530

    memo done with condition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import react,{useState,useMemo} from 'react'
 
 function App() {
+  const [count,setCount]=useState(0)
+  const [item,setItem]=useState(10)
+  const multiCountMemo=useMemo(function multiCount(){
+    console.warn("multiCount")
+    return count*2
+  },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>count:{count}</h2>
+      <h2>item:{item}</h2>
+      <h2>{multiCountMemo}</h2>
+      <button onClick={()=>setCount(count+1)}>Update Count</button>
+      <button onClick={()=>setItem(item*5)}>Update Item</button>
+
     </div>
   );
 }
